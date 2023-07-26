49d4b64d2943dcd6a7ca1cdce30bc8458585a8cd
Metlukhin <k.metlukhin@growapp.biz>
2023-07-26 09:50:15 +0300

    Complete grounds screen

--- app/src/main/java/com/zlsp/ppsphb/ui/screens/authority/AuthorityScreen.kt
+++ app/src/main/java/com/zlsp/ppsphb/ui/screens/authority/AuthorityScreen.kt
@@ -1,6 +1,14 @@
 package com.zlsp.ppsphb.ui.screens.authority
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -16,6 +24,10 @@ import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,8 +54,10 @@ fun AuthorityScreen(
                 items = state.listAuthority,
                 key = { it.id }
             ) {
+                var isExpand by remember { mutableStateOf(false) }
                 Spacer(Modifier.height(10.dp))
                 ViewCardCustomElevation(
+                    modifier = Modifier.clickable { isExpand = !isExpand },
                     border = BorderStroke(1.dp, Theme.colors.secondary)
                 ) {
                     Column(
@@ -63,43 +77,51 @@ fun AuthorityScreen(
                             style = Theme.typography.titleArticle.copy(textAlign = TextAlign.Center),
                             color = Theme.colors.onBackground
                         )
-                        it.listArticles.forEach { article ->
-                            Spacer(Modifier.height(10.dp))
-                            Divider(color = Theme.colors.onBackground.copy(0.1f))
-                            Spacer(Modifier.height(10.dp))
-                            Text(
-                                text = article.titleArticle,
-                                style = Theme.typography.titleArticle,
-                                color = Theme.colors.onBackground
-                            )
-                            Spacer(Modifier.height(10.dp))
-                            Divider(color = Theme.colors.onBackground.copy(0.1f))
-                            article.listParts.forEach { part ->
-                                Spacer(Modifier.height(10.dp))
-                                Text(
-                                    text = part.partText,
-                                    style = Theme.typography.titlePart,
-                                    color = Theme.colors.onBackground
-                                )
-                                part.points?.let { points ->
-                                    points.forEach { point ->
-                                        Spacer(Modifier.height(5.dp))
-                                        Card(
-                                            modifier = Modifier.fillMaxWidth(),
-                                            backgroundColor = Theme.colors.onBackground.copy(0.1f),
-                                            shape = RoundedCornerShape(12.dp),
-                                            elevation = 0.dp,
-                                            contentColor = Theme.colors.onBackground
-                                        ) {
-                                            Box(
-                                                Modifier
-                                                    .fillMaxWidth()
-                                                    .padding(10.dp)
-                                            ) {
-                                                Text(
-                                                    text = point,
-                                                    style = Theme.typography.textBody
-                                                )
+                        AnimatedVisibility(
+                            visible = isExpand,
+                            enter = expandVertically(),
+                            exit = shrinkVertically()
+                        ) {
+                            Column(Modifier.fillMaxWidth()) {
+                                it.listArticles.forEach { article ->
+                                    Spacer(Modifier.height(10.dp))
+                                    Divider(color = Theme.colors.onBackground.copy(0.1f))
+                                    Spacer(Modifier.height(10.dp))
+                                    Text(
+                                        text = article.titleArticle,
+                                        style = Theme.typography.titleArticle,
+                                        color = Theme.colors.onBackground
+                                    )
+                                    Spacer(Modifier.height(10.dp))
+                                    Divider(color = Theme.colors.onBackground.copy(0.1f))
+                                    article.listParts.forEach { part ->
+                                        Spacer(Modifier.height(10.dp))
+                                        Text(
+                                            text = part.partText,
+                                            style = Theme.typography.titlePart,
+                                            color = Theme.colors.onBackground
+                                        )
+                                        part.points?.let { points ->
+                                            points.forEach { point ->
+                                                Spacer(Modifier.height(5.dp))
+                                                Card(
+                                                    modifier = Modifier.fillMaxWidth(),
+                                                    backgroundColor = Theme.colors.onBackground.copy(0.1f),
+                                                    shape = RoundedCornerShape(12.dp),
+                                                    elevation = 0.dp,
+                                                    contentColor = Theme.colors.onBackground
+                                                ) {
+                                                    Box(
+                                                        Modifier
+                                                            .fillMaxWidth()
+                                                            .padding(10.dp)
+                                                    ) {
+                                                        Text(
+                                                            text = point,
+                                                            style = Theme.typography.textBody
+                                                        )
+                                                    }
+                                                }
                                             }
                                         }
                                     }
