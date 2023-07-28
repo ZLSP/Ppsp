21e4b16c87ff012b37bd1c4bd6c8e0e4ac75c711
Metlukhin <k.metlukhin@growapp.biz>
2023-07-28 20:21:24 +0300

    Fix crash scroll authority, fix orthography errors, disabled request mock server

--- app/src/main/java/com/zlsp/ppsphb/data/repository/main/MainRepositoryImpl.kt
+++ app/src/main/java/com/zlsp/ppsphb/data/repository/main/MainRepositoryImpl.kt
@@ -6,7 +6,6 @@ import com.zlsp.ppsphb.data.utils.AssetJsonReader
 import com.zlsp.ppsphb.data.utils.CacheManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onEach
@@ -21,11 +20,9 @@ class MainRepositoryImpl @Inject constructor(
 
     override fun getAppData(): Flow<GetAppDataResponse> {
         val cache = cacheManager.get()
-        return flow { cache ?: emit(dataSource.getAppData()) }
+        return flow { cache ?: emit(assetJsonReader.getLocalAppData()) }
             .onEach { appData ->
                 cache?.let { cacheManager.put(appData) }
-            }.catch {
-                emit(assetJsonReader.getLocalAppData())
             }.flowOn(Dispatchers.IO)
 
     }
